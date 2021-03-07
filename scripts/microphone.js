7fca3d1b204170bda5d951547f3544336767faea
Jeremy Gustine <jeremy@crafted.solutions>
2021-03-06 19:40:12 -0700

    notes

--- scripts/microphone.js
+++ scripts/microphone.js
@@ -4,6 +4,7 @@ function Microphone (_fft) {
   this.volume = this.vol = 0
   this.peak_volume = 0
   var self = this
+  //   var audioContext = new AudioContext({ sampleRate: 48000 * 2 })
   var audioContext = new AudioContext()
   var SAMPLE_RATE = audioContext.sampleRate
 
@@ -34,7 +35,8 @@ function Microphone (_fft) {
       var analyser = context.createAnalyser()
       analyser.smoothingTimeConstant = 0.2
       analyser.fftSize = FFT_SIZE
-      var node = context.createScriptProcessor(FFT_SIZE * 2, 1, 1)
+      //   var node = context.createScriptProcessor(FFT_SIZE * 2, 1, 1) //JAG - I'm not sure why he put FFT_SIZE * 2?
+      var node = context.createScriptProcessor(FFT_SIZE, 1, 1)
       node.onaudioprocess = function () {
         // bitcount returns array which is half the FFT_SIZE
         self.spectrum = new Uint8Array(analyser.frequencyBinCount)
@@ -42,6 +44,19 @@ function Microphone (_fft) {
         analyser.getByteFrequencyData(self.spectrum)
         // getByteTimeDomainData gets volumes over the sample time
         // analyser.getByteTimeDomainData(self.spectrum);
+
+        //JAG - I want to log out which frequency bin had the spike
+        var spikeValue = 0
+        var spikeIndex = 0
+        for (i = 0; i < self.spectrum.length; i++) {
+          if (self.spectrum[i] > spikeValue) {
+            spikeValue = self.spectrum[i]
+            spikeIndex = i
+          }
+        }
+        console.log('Largest spike value = ' + spikeValue)
+        console.log('Largest spike index = ' + spikeIndex)
+        console.log(self.spectrum)
 
         self.vol = self.getRMS(self.spectrum)
         // get peak - a hack when our volumes are low
@@ -110,12 +125,12 @@ function clamp (value, min, max) {
 }
 
 document.body.addEventListener('click', function () {
-  var Mic = new Microphone()
+  var Mic = new Microphone(1024 * 16)
 
   var ctx = createCanvas('canvas1')
   // make a grid 200 wide
   // I covered how to make grids here
-  var grid = new Grid(200, 1)
+  var grid = new Grid(512, 1)
   function draw () {
     //ctx.background(235);
     ctx.clearRect(0, 0, w, h)
@@ -132,3 +147,57 @@ document.body.addEventListener('click', function () {
   Mic.init()
   draw()
 })
+
+//https://stackoverflow.com/questions/30013898/what-is-the-range-of-the-values-returned-by-analyser-getbytefrequencydata-in-the
+//https://stackoverflow.com/questions/14789283/what-does-the-fft-data-in-the-web-audio-api-correspond-to/14789992#14789992
+/*
+According to the above links, the output of getByteFrequencyData is half the length of the FFT_SIZE
+
+each bin refers to freqs:
+N * (samplerate/fft_size)
+
+
+So on chrome:
+bin_width = (48000/1024) = 46.875
+N * (bin_width)
+
+46.875 is WAY too big for a guitar tuner
+
+
+=========================================
+
+//https://electronics.stackexchange.com/questions/12407/what-is-the-relation-between-fft-length-and-frequency-resolution
+
+number of bins = number of samples / 2 (aka fft_size / 2)
+
+"We can see from the above that to get smaller FFT bins we can either run a longer FFT (that is, 
+take more samples at the same rate before running the FFT) or decrease our sampling rate."
+
+
+=====================================
+If I did 48000 / 8192, I would get a freq resolution of 5.8.  Not....awful
+48000 / 16384 = 2.92.  Maybe try this?
+Or...try lowering sample rate?
+
+
+
+
+Test:
+Sample rate = 48000
+fft size = 16384
+E spike was in bin 55/56
+
+So...
+Frequency bin width = 2.92
+55 * 2.92 = 160.6
+56 * 2.92 = 163.52
+
+That is super close in that it is twice as much as the expected frequency of E, which is 82.41
+
+
+Test 2:
+High E string (expected freq of 329.63 Hz)
+I definitely saw the harmonic spikes...sometimes highest spike was 225 and sometimes 338
+225 * 2.92 = 657
+Again...twice as much as expected frequency of high E
+*/
