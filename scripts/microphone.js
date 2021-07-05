f2171d58efcfc5b1e60ab057328ad246532c96ac
jeremygustine <jeremy@crafted.solutions>
2021-07-05 10:27:48 -0600

    Clean up comments

--- scripts/microphone.js
+++ scripts/microphone.js
@@ -4,20 +4,13 @@ function Microphone (_fft) {
   this.volume = this.vol = 0
   this.peak_volume = 0
   var self = this
+  
   var audioContext = new AudioContext({ sampleRate: (48000 / 2) / 2 })
-  // var audioContext = new AudioContext()
   var SAMPLE_RATE = audioContext.sampleRate
 
-  // this is just a browser check to see
-  // if it supports AudioContext and getUserMedia
   window.AudioContext = window.AudioContext || window.webkitAudioContext
-  navigator.getUserMedia =
-    navigator.getUserMedia || navigator.webkitGetUserMedia
+  navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia
 
-  // now just wait until the microphone is fired up
-  // window.addEventListener('load', init, false);
-
-  //document.body.addEventListener('click', init) //I had to add this line instead - JAG
 
   this.init = function () {
     try {
@@ -31,21 +24,15 @@ function Microphone (_fft) {
   function startMic (context) {
     navigator.getUserMedia({ audio: true }, processSound, error)
     function processSound (stream) {
-      // analyser extracts frequency, waveform, etc.
       var analyser = context.createAnalyser()
       analyser.smoothingTimeConstant = 0.2
       analyser.fftSize = FFT_SIZE
-      //   var node = context.createScriptProcessor(FFT_SIZE * 2, 1, 1) //JAG - I'm not sure why he put FFT_SIZE * 2?
       var node = context.createScriptProcessor(FFT_SIZE, 1, 1)
-      node.onaudioprocess = function () {
-        // bitcount returns array which is half the FFT_SIZE
-        self.spectrum = new Uint8Array(analyser.frequencyBinCount)
-        // getByteFrequencyData returns amplitude for each bin
-        analyser.getByteFrequencyData(self.spectrum)
-        // getByteTimeDomainData gets volumes over the sample time
-        // analyser.getByteTimeDomainData(self.spectrum);
 
-        //JAG - I want to log out which frequency bin had the spike
+      node.onaudioprocess = function () {
+        self.spectrum = new Uint8Array(analyser.frequencyBinCount)
+        analyser.getByteFrequencyData(self.spectrum)
+
         interpret(self.spectrum, SAMPLE_RATE, FFT_SIZE)
 
         self.vol = self.getRMS(self.spectrum)
@@ -53,11 +40,13 @@ function Microphone (_fft) {
         if (self.vol > self.peak_volume) self.peak_volume = self.vol
         self.volume = self.vol
       }
+
       var input = context.createMediaStreamSource(stream)
       input.connect(analyser)
       analyser.connect(node)
       node.connect(context.destination)
     }
+
     function error () {
       console.log(arguments)
     }
@@ -91,8 +80,7 @@ function Microphone (_fft) {
       return 0
     }
   }
-  //////// SOUND UTILITIES  ////////
-  ///// ..... we going to put more stuff here....
+
   return this
 }
 
